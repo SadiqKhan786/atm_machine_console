ca992c1d0bf915a014664e97424c95ddc5f1289b
Sadiq Khan <geniuskhan96@gmail.com>
2024-04-02 01:23:34 +0500

    project complete

---
+++ index.js
@@ -0,0 +1,61 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin number",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("correct pin code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "select option",
+            type: "list",
+            choices: ["withdraw", "check balance", "fast cash"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log(`your balance is ${myBalance}`);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(`your balance is ${myBalance}`);
+    }
+    else if (operationAns.operation === "fast cash") {
+        console.log("choose your option");
+        var fastcashAns = await inquirer.prompt([
+            {
+                name: "opfastcash",
+                message: "select option",
+                type: "list",
+                choices: ["1000", "2000", "3000"]
+            }
+        ]);
+    }
+    if (fastcashAns.opfastcash === "1000") {
+        myBalance -= fastcashAns.opfastcash;
+        console.log(`your new balance is ${myBalance}`);
+    }
+    else if (fastcashAns.opfastcash === "2000") {
+        myBalance -= fastcashAns.opfastcash;
+        console.log(`your new balance is ${myBalance}`);
+    }
+    else if (fastcashAns.opfastcash === "3000") {
+        myBalance -= fastcashAns.opfastcash;
+        console.log(`your new balance is ${myBalance}`);
+    }
+}
+else {
+    console.log("enter correct pin");
+}
